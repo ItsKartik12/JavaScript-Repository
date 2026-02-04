fa6b28d040754c9e9939eb8ba650245fc3a1ff17
ItsKartik093 <kg0192421@gmail.com>
2026-02-04 18:00:29 +0000

    container config done

---
+++ 01_Basics/LetVarChar.js
@@ -0,0 +1,8 @@
+const accountId = 54545;
+let accountEmail = "Kg0192421@gmail.com";
+var accountPassword = "ItsKartik";
+let accountCity = "Delhi";
+accountEmail = "ItsKartik1234@gmail.com";
+//accountId = 68567;
+accountPassword = "ItsMyName";
+console.table([accountId,accountEmail,accountPassword,accountCity]);
