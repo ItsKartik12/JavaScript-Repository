d7b6f34626e0e105e85551f871a9a305531ab16c
ItsKartik093 <kg0192421@gmail.com>
2026-02-05 17:35:32 +0000

    Abhi pura nhi hua

---
+++ 01_Basics/05_Operations.js
@@ -0,0 +1,14 @@
+//******************************* Operations *******************************\\
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue); // -3
+// console.log(2**3); // 8 
+// let str = "Hello";
+// let str2 = "World";
+// let str3 = str + " " + str2;
+// console.log(str3); // "Hello World"
+// console.log("1" + 2 + 2);
+// console.log(2+2+"1");
+console.log(true);
+console.log(+true); // 1
+console.log(+false); // 0 
