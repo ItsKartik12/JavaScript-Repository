b52f393a2af92f303f7d0be1f39d88b23dca8a72
ItsKartik093 <kg0192421@gmail.com>
2026-02-10 19:04:25 +0000

    final

---
+++ 01_Basics/09_Nums&Maths.js
@@ -0,0 +1,11 @@
+const score = 400;
+const newNumber = new Number(100);
+
+console.log(score);
+console.log(newNumber);
+
+console.log(typeof score);
+console.log(typeof newNumber);
+console.log(newNumber.toString());
+console.log(newNumber.toString().length);
+console.log(newNumber.toFixed(2));
