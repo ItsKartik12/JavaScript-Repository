e182bb7db7cb7e446084b0e3d1681c106560aa13
ItsKartik093 <kg0192421@gmail.com>
2026-02-12 17:48:26 +0000

    final

--- 01_Basics/09_Nums&Maths.js
+++ 01_Basics/09_Nums&Maths.js
@@ -1,11 +1,29 @@
-const score = 400;
-const newNumber = new Number(100);
+const score = 400
+// console.log(score);
 
-console.log(score);
-console.log(newNumber);
+const balance = new Number(100)
+// console.log(balance);
 
-console.log(typeof score);
-console.log(typeof newNumber);
-console.log(newNumber.toString());
-console.log(newNumber.toString().length);
-console.log(newNumber.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.max(1, 5, 3, 9, 2));
+console.log(Math.min(1, 5, 3, 9, 2));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 6) + 1); // random number between 1 and 10
+const max = 6;
+const min = 1;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random number between 1 and 10
