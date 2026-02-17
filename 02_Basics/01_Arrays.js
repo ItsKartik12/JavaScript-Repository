9d01d4cbaea78265cbba7a64b7a680cdf18b5dcf
ItsKartik093 <kg0192421@gmail.com>
2026-02-17 12:32:22 +0000

    final

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,19 @@
+// Arrays
+const myArray = [1, 2, 3, 4, 5];
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+// console.log(myArray);
+// console.log(myArr2);
+
+// Arrays methods
+myArray.push(6);
+myArray.push(7);
+myArray.push(20);
+// console.log(myArray);
+//if there is a error we can use pop to remove the last element
+myArray.pop();
+//console.log(myArray);
+myArray.unshift(9);
+//console.log(myArray);
+myArray.shift();
+//console.log(myArray);
