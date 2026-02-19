c2e55ce15627a650ede2fce1d1d7b4e3d7bd07b2
ItsKartik093 <kg0192421@gmail.com>
2026-02-19 18:33:29 +0000

    m

--- 02_Basics/01_Arrays.js
+++ 02_Basics/01_Arrays.js
@@ -17,3 +17,14 @@ myArray.unshift(9);
 //console.log(myArray);
 myArray.shift();
 //console.log(myArray);
+
+// console.log(myArray.includes(3)); // true
+// console.log(myArray.includes(10)); // false
+// console.log(myArray.indexOf(3)); // 2
+const newArr = myArray.join();
+console.log(newArr); // "1,2,3,4,5,6,7,20"
+console.log(myArray); // [1, 2, 3, 4, 5, 6, 7, 20]
+//slice , splice
+const mynarr1 = myArray.slice(1,3);
+console.log("A ",mynarr1);
+console.log("B",myArray);
