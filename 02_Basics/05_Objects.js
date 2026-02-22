f2ee2e12f35d1254e797c2766793d116a33d1566
ItsKartik093 <kg0192421@gmail.com>
2026-02-22 18:19:52 +0000

    final

---
+++ 02_Basics/05_Objects.js
@@ -0,0 +1,12 @@
+const course = {
+    courseName: "Js full Course",
+    price: 999,
+    courseInstructor: "Hitesh Choudhary"
+
+}
+
+// course.courseInstructor
+const { courseInstructor: Instructor } = course;
+//console.log(courseInstructor);
+console.log(Instructor);
+
