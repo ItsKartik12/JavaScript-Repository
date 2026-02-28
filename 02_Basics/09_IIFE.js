851e01cdd33b31c3ccba43f40a0a9a549e7cfb20
ItsKartik093 <kg0192421@gmail.com>
2026-02-28 09:50:34 +0000

    final

---
+++ 02_Basics/09_IIFE.js
@@ -0,0 +1,8 @@
+// IIFE - Immediately Invoked Function Expression
+(function code() {
+    console.log('DB CONNECTED');
+
+})(); // IIFE is defined and immediately invoked
+(() => {
+    console.log('DB CONNECTED with arrow function');
+})(); // IIFE with arrow function
