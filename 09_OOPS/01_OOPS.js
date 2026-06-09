d6f0f2400f3bb56a83924c331bae27fbd014666f
ItsKartik093 <kg0192421@gmail.com>
2026-06-09 18:10:49 +0000

    final

--- 09_OOPS/01_OOPS.js
+++ 09_OOPS/01_OOPS.js
@@ -1,4 +1,4 @@
-const user = {
+const user12 = {
     userName: "Kartik",
     loginCount: 8,
     signedIn: true,
@@ -8,6 +8,20 @@ const user = {
 
     }
 }
-console.log(user.userName);
-console.log(user.getUserDetails());
+console.log(user12.userName);
+console.log(user12.getUserDetails());
 
+
+function User(username, loginCount, signedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.signedIn = signedIn
+    return this;
+}
+
+const user1 = new User("Kartik", 8, true);
+const user2 = new User("Rahul", 5, false);
+console.log(user1);
+console.log(user2);
+console.log(user1.constructor);
+console.log(user1 instanceof User);
