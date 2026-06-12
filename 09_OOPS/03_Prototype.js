b368b3cdeb4f8eedefebeb62b6f225c528573ab9
ItsKartik093 <kg0192421@gmail.com>
2026-06-12 13:33:00 +0000

    final

--- 09_OOPS/03_Prototype.js
+++ 09_OOPS/03_Prototype.js
@@ -1,1 +1,21 @@
 let myName = "Kartik";
+
+let myHeroes = ["Superman", "Batman", "Spiderman"];
+let heroPower = {
+    Superman: "Super Strength",
+    Batman: "Rich",
+    Spiderman: "Spider Sense",
+    getSpidy: function () {
+        return `Spiderman is the best Hero.\nHis superpower is ${this.Spiderman}`;
+    }
+}
+
+Object.prototype.kartik = function () {
+    return "This is a property added to the Object prototype.";
+}
+
+console.log(myName.kartik());
+console.log(myHeroes.kartik());
+console.log(heroPower.kartik());
+console.log(heroPower.Spiderman);
+console.log(heroPower.getSpidy());
