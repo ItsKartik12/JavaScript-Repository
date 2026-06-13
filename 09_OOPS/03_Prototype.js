d0df528f248f5648aa44514f22dbfa9bd2df4629
ItsKartik093 <kg0192421@gmail.com>
2026-06-13 09:29:17 +0000

    final

--- 09_OOPS/03_Prototype.js
+++ 09_OOPS/03_Prototype.js
@@ -19,3 +19,45 @@ console.log(myHeroes.kartik());
 console.log(heroPower.kartik());
 console.log(heroPower.Spiderman);
 console.log(heroPower.getSpidy());
+myHeroes.push("Ironman");
+console.log(myHeroes.kartik());
+
+Array.prototype.getLastHero = function () {
+    return this[this.length - 1];
+}
+
+console.log(myHeroes.getLastHero());
+myHeroes.pop();
+myHeroes.push("Thor");
+console.log(myHeroes.getLastHero());
+
+// heroPower.getLastHero();//getLastHero is only  exist in array myHeroes not in object
+
+//Inheritance
+const User = {
+    name: "Kartik",
+    email: "kg0192421@gmail.com"
+}
+const teacher = {
+    makeVideoes: true
+}
+const TeachingSupport = {
+    isAvailable: false
+}
+const TASupport = {
+    makeAssignment: 'JS Assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+teacher.__proto__ = User;
+//Modern Syntax
+Object.setPrototypeOf(TeachingSupport, teacher);
+console.log(User);
+
+let anotherUsername = "Kartik     ";
+String.prototype.trueLength = function () {
+    console.log(`${this}`);
+    console.log(`${this.name}`),
+        console.log(`True length is: ${this.trim().length}`)
+}
+anotherUsername.trueLength();
